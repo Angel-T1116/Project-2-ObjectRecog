2481b1af4ff7cbe83682184f92e6502e87ff43a4
cocobyte1 <123109591+cocobyte1@users.noreply.github.com>
2025-04-21 20:40:24 -0400

    updated with guidance on using pydoc

--- YOLO11/main.py
+++ YOLO11/main.py
@@ -2,19 +2,27 @@ from ultralytics import YOLO
 import torch
 import cv2
 import os
+import pydoc
 
+"""
+This function converts the video output from ultralytics in a .avi format to .mp4
+"""
 def videoConverter(inputPath, outputPath):
     """Converts video in .avi format to .mp4"""
     
+    # convert the video into a cv2 input
     vid = cv2.VideoCapture(inputPath)
 
+    # get data from the video
     fps = vid.get(cv2.CAP_PROP_FPS)
     width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
     vid_size = (width, height)
 
+    # create an output file in mp4 format
     output = cv2.VideoWriter(outputPath, cv2.VideoWriter_fourcc(*'mp4v'), fps, vid_size)
 
+    # write the frames into the output video
     while(True):
         ret, frame = vid.read()
         if not (ret):
@@ -44,3 +52,6 @@ inputPath = r"runs\detect\predict\IMG_4400.avi"
 outputPath = r"runs\detect\predict\results.mp4"
 
 videoConverter(inputPath, outputPath)
+
+# generate pydoc documentation file
+pydoc.writedoc('main')
--- YOLO11/readme.md
+++ YOLO11/readme.md
@@ -2,3 +2,15 @@ install python virtual environment
 use pip install ultralytics
 download yolo11x.pt model into weights directory
 run
+
+using pydoc:
+pydoc is already included in standard libraries. Make sure to comment code using """ documentation here """.
+
+to use in a module 
+import pydoc
+
+pydoc.writedoc('name_of_file')
+
+to use in CLI
+python -m pydoc -w <name_of_file>
+
