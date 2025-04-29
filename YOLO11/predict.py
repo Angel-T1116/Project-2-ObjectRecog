ba3aa07eff93752bbe0dfeb97c2cb97166d9fa78
cocobyte1 <coco@bytefamily.com>
2025-04-29 09:32:13 -0400

    fixed

--- YOLO11/predict.py
+++ YOLO11/predict.py
@@ -2,9 +2,41 @@ from ultralytics import YOLO
 import cv2
 import os
 import numpy as np
-from videoConverter import videoConverter
-import pydoc
 
+import pydoc
+# TO DO: clear previous predict runs
+#TO DO: take output path from results and use as input for videoConverter
+
+"""
+This function converts the video output from ultralytics in a .avi format to .mp4
+"""
+def CustomVideoConverter(inputPath, outputPath):
+    """Converts video in .avi format to .mp4"""
+    
+    # convert the video into a cv2 input
+    vid = cv2.VideoCapture(inputPath)
+
+    # get data from the video
+    fps = vid.get(cv2.CAP_PROP_FPS)
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    vid_size = (width, height)
+
+    # create an output file in mp4 format
+    output = cv2.VideoWriter(outputPath, cv2.VideoWriter_fourcc(*'mp4v'), fps, vid_size)
+
+    # write the frames into the output video
+    while(True):
+        ret, frame = vid.read()
+        if not (ret):
+            break
+        output.write(frame)
+
+    vid.release()
+    output.release()
+
+# pydoc.writedoc('videoConverter')
+        
 # Load a model
 model = YOLO("weights/best.pt")  # load a trained model
 
@@ -13,7 +45,7 @@ model = YOLO("weights/best.pt")  # load a trained model
 # results = model.predict("IMG_4400.mp4", show=True, stream=True, save=True, device=0)  # predict on an image
 
 # predict for images in directory
-results = model.predict(source="datasets/Lightsabers.v1i.yolov11/test/images", project="runs/detect", name="predict", save=True, device=0)  # predict on an image
+results = model.predict(source="datasets/IMG_4400.mp4", project="runs/detect", name="predict", stream=True, show=True, save=True, device=0)  # predict on an image
 
 
 # Access the results
@@ -25,11 +57,11 @@ for result in results:
     names = [result.names[cls.item()] for cls in result.boxes.cls.int()]  # class name of each box
     confs = result.boxes.conf  # confidence score of each box
 
-inputPath = r"runs\detect\predict\IMG_4400.avi" # edit this as necessary
-outputPath = r"runs\detect\predict\results.mp4"
+inputPath = "YOLO11/runs/detect/predict/IMG_4400.avi" # edit this as necessary to the expected path of the output video
+outputPath ="YOLO11/runs/detect/predict/results.mp4"  
 
 """ run video conversion using openCV"""
-videoConverter(inputPath, outputPath)
+CustomVideoConverter(inputPath, outputPath)
 
 # generate pydoc documentation file
-pydoc.writedoc('predict')
+# pydoc.writedoc('predict')
