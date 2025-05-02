02170c270cfb4606a2515372600c5d059a2ca1b2
cocobyte1 <coco@bytefamily.com>
2025-05-01 20:45:50 -0400

    updated documentation

--- YOLO11/test_clean_runs.py
+++ YOLO11/test_clean_runs.py
@@ -3,6 +3,7 @@ import os
 from YOLO11.predict import clean_runs
 
 def test_clean_runs():
+    """ test for function that clears old prediction runs"""
     testPath = "testDir"
     os.makedirs(testPath, exist_ok=True)
 
