1d35d16d39cf0b2ef33bdebe21b9da23b3eefa55
cocobyte1 <coco@bytefamily.com>
2025-05-01 20:31:46 -0400

    updated to not run during documentation

--- YOLO11/train.py
+++ YOLO11/train.py
@@ -1,14 +1,15 @@
 from ultralytics import YOLO
 import torch 
 
-""" File containing training parameters used to generate model"""
-model = YOLO("yolo11x.pt") # load pretrained model
+if __name__ == "__main__":
+    """ File containing training parameters used to generate model"""
+    model = YOLO("yolo11x.pt") # load pretrained model
 
-# train model using ultralytics
-results = model.train(data="data.yaml", # ensure you have the dataset downloaded before using this
-                        project="runs/detect", # path to place runs in
-                        name="train", # folder within the project
-                        epochs=100, 
-                        imgsz=640,
-                        batch=4, # reduce batch size for smaller GPU 
-                        device=0) # device=0 for GPU training
+    # train model using ultralytics
+    results = model.train(data="data.yaml", # ensure you have the dataset downloaded before using this
+                            project="runs/detect", # path to place runs in
+                            name="train", # folder within the project
+                            epochs=100, 
+                            imgsz=640,
+                            batch=4, # reduce batch size for smaller GPU 
+                            device=0) # device=0 for GPU training
